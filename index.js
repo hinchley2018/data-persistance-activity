808ff93249770fa4039937ee4be2bb50c165d451
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2021-08-11 07:47:08 -0500

    finish add to cart function

--- index.js
+++ index.js
@@ -17,5 +17,14 @@ userH1.textContent = userName;
 //Note: we haven't learned about these yet, but its just code that runs when "Add to cart" button is clicked
 function addToCart(title, price, picturePath){
     alert(`${title} added to cart`);
-    //Task 3: add the item (title,price,picturePath) to localStorage productsInCart array
+    //Note: this grabs the current cart from localStorage
+    let productsInCart = JSON.parse(localStorage.getItem("productsInCart"))
+    //Note: this handles empty cart
+    if (productsInCart === null || productsInCart === undefined){
+        productsInCart = [];
+    }
+    //Task 3: add the item (title,price,picturePath) as an object to productsInCart array
+    
+    //Task 3: store updated productsInCart array in localStorage (remember to stringify)
+    
 }
